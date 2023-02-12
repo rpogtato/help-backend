38c08660e1f2d41bb0e2e1ef7b7f5f171198698a
Pogtato <erikdeltaco@gmail.com>
2023-02-12 20:22:59 +0100

    delete request

--- routes/users.js
+++ routes/users.js
@@ -11,11 +11,11 @@ const router = express.Router();
 /* READ */
 router.get("/", getAllUsers);
 router.get("/:id", getUser);
-router.get("/:id/delete", deleteUser);
 
 /* CREATE */
 router.post("/register", register);
 
 /* DELETE */
+router.delete("/:id/delete", deleteUser);
 
 export default router;
